e263da89669f68e71bb0510cafbb1d0ec4280607
saikat1095 <mdabuthaersaikat@gmail.com>
2021-07-04 10:13:35 +0600

    may will work

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ require("dotenv").config();
 const bodyParser = require("body-parser");
 
 const app = express();
-app.use(cors({origin : process.env.CLIENT_URL}));
+// app.use(cors({origin : process.env.CLIENT_URL}));
+app.use(cors());
 app.use(bodyParser.json());
 
 app.use("/educations", educationRoutes);
